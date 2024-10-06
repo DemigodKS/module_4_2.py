8f46af284423c90a3c163c41fcb560453f8e9b74
DemigodKS <mankok222@gmail.com>
2024-10-07 00:10:43 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -1,4 +1,3 @@
-
 # вызов inner_function внутри функции
 def test_function ():
     def  inner_function():
@@ -8,12 +7,9 @@ def test_function ():
 test_function()
 
 # вызов inner_function вне функции
-def test_function():
-    def inner_function():
-        global a
+def test_function ():
+    def  inner_function():
         a = 'Я в области видимости функции test_function'
-    inner_function()
+        print(a)
+inner_function() #NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
 test_function()
-print(a)
-
-
